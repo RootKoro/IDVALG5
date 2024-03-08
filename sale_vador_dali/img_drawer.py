d5621285161667b1a0444e20b9d423d727e658d4
Cyr Mathieu <gueye_d@etna-alternance.net>
2024-03-08 16:36:50 +0100

    update: removed the grey and replaced it with the pixel calculation
    update: adjusted the gaussian blur

--- sale_vador_dali/img_drawer.py
+++ sale_vador_dali/img_drawer.py
@@ -1,16 +1,17 @@
-# Author: Cyr Mathieu GUEYE, Maïssane QASMI, Dona DOSSA, Hacene SADOUDI# Author: Cyr Mathieu GUEYE
+# Author: Cyr Mathieu GUEYE, Maïssane QASMI, Dona DOSSA, Hacene SADOUDI
 # Licenceless
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 
+from numpy import median
 from sys import argv
 
 from cv2 import (
     COLOR_BGR2GRAY,
     GaussianBlur,
-    bitwise_not,
+    Canny,
     cvtColor,
+    bitwise_not,
     destroyAllWindows,
-    divide,
     imread,
     imshow,
     waitKey,
@@ -20,7 +21,7 @@ from cv2 import (
 class ImgDrawer:
     """
     Module: IDV-ALGO5
-    Step: 01
+    Step: 02
     Goal: Draw a sketch
     """
 
@@ -31,38 +32,26 @@ class ImgDrawer:
         self.img_path = img_path
         self.k_size = k_size
 
-    def img_transformer(self) -> tuple:
+    def sketch_edge_drawer(self) -> tuple:
         """
-        1- Convert the image to grey
-        2- Invert the converted image
-        3- Blur the inverted image
-        4- invert the blurred image
-
-        Params:
-        -------
-            img_path : string
-                path to the image
-            k_size : int
-                kernel size ; which might defer depending on the image ;
-                on small images, the kernel size should be very small
-                and for large images, the kernel size should be very large
+        1- Read the image
+        2- Check if the k_size:
+            a- k_size is given: image is blured with the gaussian algorithm
+            b- k_size is not given: image is not blured, we go on the next step
+        3- Get the median value of the img
+            The image is converted into a 2 dimensional array of bits
+            the median is retrieved from that array
+        4- draw a new 2 dimensional array of bits containing the points
+        retreived from the calculation of Canny algorithm
+        5- Show the new img corresponding to the new array
         """
         img = imread(self.img_path)
-        grey_img = cvtColor(img, COLOR_BGR2GRAY)
-        inverted_img = bitwise_not(grey_img)
-        blurred_img = GaussianBlur(inverted_img, (self.k_size, self.k_size), 0)
-        inverted_blurred_img = bitwise_not(blurred_img)
+        if self.k_size > 0:
+            img = GaussianBlur(img, (self.k_size, self.k_size), 0)
+        median_value = median(img)
 
-        return (grey_img, inverted_blurred_img)
+        sketch_img = bitwise_not(Canny(img, median_value, 255))
 
-    def sketch_edge_drawer(self):
-        grey_img, inverted_blurred_img = self.img_transformer()
-        sketch_img = divide(grey_img, inverted_blurred_img, scale=256.0)
-
-        # Save Sketch
-        # cv2.imwrite("sketch.png", sketch_img)
-
-        # Display sketch
         imshow("sketch image", sketch_img)
         waitKey(0)
         destroyAllWindows()
@@ -70,23 +59,17 @@ class ImgDrawer:
 
 def main():
     try:
-        if len(argv) > 2:
-            bfl = ImgDrawer(argv[1], int(argv[2]))
-            bfl.sketch_edge_drawer()
+        if len(argv) == 2:
+            svd = ImgDrawer(argv[1], -1)
+        elif len(argv) > 2:
+            svd = ImgDrawer(argv[1], int(argv[2]))
         else:
-            raise Exception("""Please specify an image path and a kernel size.""")
+            raise Exception("")
+
+        svd.sketch_edge_drawer()
     except Exception as e:
         print(e)
-        print(
-            """
-Retry while checking the following steps:
-    the first argument is a valid image path
-    the second argument is an integer
-    The integer given as second argument should be proportional o the image
-
-ex. big_fernand_leger.py assets/img.png 5
-            """
-        )
+        print("""Please specify a valid image path and a kernel size.""")
 
 
 if __name__ == "__main__":
