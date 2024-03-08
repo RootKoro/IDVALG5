e2efbf3c649564c17df8d4b0b72091bfcd17aac8
Cyr Mathieu <gueye_d@etna-alternance.net>
2024-03-08 11:07:02 +0100

    update: updated the code into points detections instead of greying the image

--- big_fernand_leger/img_drawer.py
+++ big_fernand_leger/img_drawer.py
@@ -2,15 +2,13 @@
 # Licenceless
 # Permission is hereby granted, free of charge, to any person obtaining a copy of this software
 
+from numpy import median
 from sys import argv
 
 from cv2 import (
-    COLOR_BGR2GRAY,
-    GaussianBlur,
+    Canny,
     bitwise_not,
-    cvtColor,
     destroyAllWindows,
-    divide,
     imread,
     imshow,
     waitKey,
@@ -25,39 +23,24 @@ class ImgDrawer:
     """
 
     img_path: str
-    k_size: int
 
-    def __init__(self, img_path: str, k_size: int):
+    def __init__(self, img_path: str):
         self.img_path = img_path
-        self.k_size = k_size
 
-    def img_transformer(self) -> tuple:
+    def sketch_edge_drawer(self) -> tuple:
         """
-        1- Convert the image to grey
-        2- Invert the converted image
-        3- Blur the inverted image
-        4- invert the blurred image
-
-        Params:
-        -------
-            img_path : string
-                path to the image
-            k_size : int
-                kernel size ; which might defer depending on the image ;
-                on small images, the kernel size should be very small
-                and for large images, the kernel size should be very large
+        1- Read the image
+        2- Get the median value of the img
+            the image is converted into a 2 dimensional array of bits
+            the median is retrieved from that array
+        3- draw a new 2 dimensional array of bits containing the points
+            retreived from the calculation of Canny algorithm
+        4- Show the new img corresponding to the new array
         """
         img = imread(self.img_path)
-        grey_img = cvtColor(img, COLOR_BGR2GRAY)
-        inverted_img = bitwise_not(grey_img)
-        blurred_img = GaussianBlur(inverted_img, (self.k_size, self.k_size), 0)
-        inverted_blurred_img = bitwise_not(blurred_img)
+        median_value = median(img)
 
-        return (grey_img, inverted_blurred_img)
-
-    def sketch_edge_drawer(self):
-        grey_img, inverted_blurred_img = self.img_transformer()
-        sketch_img = divide(grey_img, inverted_blurred_img, scale=256.0)
+        sketch_img = bitwise_not(Canny(img, median_value, 255))
 
         imshow("sketch image", sketch_img)
         waitKey(0)
@@ -66,23 +49,13 @@ class ImgDrawer:
 
 def main():
     try:
-        if len(argv) > 2:
-            bfl = ImgDrawer(argv[1], int(argv[2]))
+        if len(argv) >= 1:
+            bfl = ImgDrawer(argv[1])
             bfl.sketch_edge_drawer()
         else:
-            raise Exception("""Please specify an image path and a kernel size.""")
-    except Exception as e:
-        print(e)
-        print(
-            """
-Retry while checking the following steps:
-    the first argument is a valid image path
-    the second argument is an integer
-    The integer given as second argument should be proportional o the image
-
-ex. img_drawer.py assets/img.png 5
-            """
-        )
+            raise Exception("")
+    except:
+        print("""Please specify a valid image path and a kernel size.""")
 
 
 if __name__ == "__main__":
